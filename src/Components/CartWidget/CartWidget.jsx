dabbbb793ca8b8cab182c575dc074d1b6a636ad5
JoacoFichter <joaquinfichter9@gmail.com>
2023-05-17 17:01:40 -0300

    cartWidget fixed

--- src/Components/CartWidget/CartWidget.jsx
+++ src/Components/CartWidget/CartWidget.jsx
@@ -7,9 +7,14 @@ const CartWidget = () => {
     const {cartList} = useCartContext()
     return (
         <div className="cartWidget">
-            <p>{cartList.reduce((acc, element)=>{
-                return acc += element.quantity                
-            }, 0)}</p>
+            {cartList[0]
+                ?
+                    <p>{cartList.reduce((acc, element)=>{
+                        return acc += element.quantity                
+                    }, 0)}</p>
+                :
+                    <></>
+            }
             <Link to={'/SoundHaven-JoaquinFichter/Cart'}><ShoppingCart width="30">holaaaa</ShoppingCart></Link>
         </div>
     )
